e55465b6a28b2ee2cdadb6f67a4528440938f9a1
reneewhite <reneilwesefora@gmail.com>
2018-07-23 09:35:02 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,26 +9,29 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+ declarations: [
+   MyApp,
+   HomePage,
+   ListPage
+ ],
+ imports: [
+   BrowserModule,
+   IonicModule.forRoot(MyApp),
+   HttpClientModule
+ ],
+ bootstrap: [IonicApp],
+ entryComponents: [
+   MyApp,
+   HomePage,
+   ListPage
+ ],
+ providers: [
+   StatusBar,
+   SplashScreen,
+   {provide: ErrorHandler, useClass: IonicErrorHandler}
+ ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import "rxjs/add/operator/map";
+import { Platform } from 'ionic-angular/platform/platform';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+ selector: 'page-home',
+ templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+ users: Observable<any>;
 
-  }
+ constructor(public navCtrl: NavController, private httpClient: HttpClient, private plt: Platform, private alertCtrl: AlertController) {
+   this.users = this.httpClient.get('https://randomuser.me/api/?results=20')
+   .map(res => res['results'])
+ }
 
+ checkPlatform() {
+   let alert = this.alertCtrl.create({
+     title: 'Platform',
+     message: 'You are running on: ' + this.plt.platforms(),
+     buttons: ['OK']
+   });
+   alert.present();
+
+   if (this.plt.is('cordova')) {
+     // Do Cordova stuff
+   } else {
+     // Do stuff inside the regular browser
+   }
+ }
 }
